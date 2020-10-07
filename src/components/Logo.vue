ced7f44ace2afd53e389f8d6820f23a74fcc1673
xixiguang <xue2097328522@gmail.com>
2020-10-07 20:55:44 +0800

    登录功能完成

---
+++ src/components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="hm-logo">
+  <span class="iconfont iconnew"></span>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.hm-logo {
+  text-align: center;
+
+  .iconfont {
+    font-size: 126px;
+    color: red;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/base.less'
+import './styles/iconfont.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+import HmHeader from './components/HmHeader.vue'
+import Logo from './components/Logo.vue'
+import axios from 'axios'
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://localhost:3000'
+Vue.use(Vant)
+Vue.component('hm-header', HmHeader)
+Vue.component('hm-logo', Logo)
 
 Vue.config.productionTip = false
 
